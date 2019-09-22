d1618a1a41b3037b4e442d0b86429a7ae27245ba
betinhoow <robertonobre90@gmail.com>
2019-09-22 10:11:29 -0300

    Spring project start

---
+++ src/main/java/com/robertonobre/comercial/ComercialUiApplication.java
@@ -0,0 +1,13 @@
+package com.robertonobre.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercialUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercialUiApplication.class, args);
+	}
+
+}
